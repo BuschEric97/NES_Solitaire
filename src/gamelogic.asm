4b1e45dfed8a417eab776ddec45c69eacaa70098
Eric Busch <buscheric97@gmail.com>
2024-01-30 18:49:09 -0600

    Prevented cursor overflow

--- src/gamelogic.asm
+++ src/gamelogic.asm
@@ -1,66 +1,80 @@
 .segment "ZEROPAGE"
 .segment "CODE"
 
+; cursor X movement limited to between $00 - $FF (inclusive)
+; cursor Y movement limited to between $07 - $E6 (inclusive)
 set_new_cursor_pos:
     lda CURSORMOVEMENT
     and #%00000001
     beq not_moving_up
         ;moving_up:
-        ldx CURSORYPOS
-        dex 
-        stx CURSORYPOS
-        txa 
-        and #%00000111
-        cmp #%00000111
-        bne not_moving_up
-            ldx CURSORTILEYPOS
+        lda CURSORYPOS
+        cmp #$07
+        beq not_moving_up
+            ldx CURSORYPOS
             dex 
-            stx CURSORTILEYPOS
+            stx CURSORYPOS
+            txa 
+            and #%00000111
+            cmp #%00000111
+            bne not_moving_up
+                ldx CURSORTILEYPOS
+                dex 
+                stx CURSORTILEYPOS
     not_moving_up:
     lda CURSORMOVEMENT
     and #%00000010
     beq not_moving_right
         ;moving_right:
-        ldx CURSORXPOS
-        inx 
-        stx CURSORXPOS
-        txa 
-        and #%00000111
-        cmp #%00000000
-        bne not_moving_right
-            ldx CURSORTILEXPOS
+        lda CURSORXPOS
+        cmp #$FF
+        beq not_moving_right
+            ldx CURSORXPOS
             inx 
-            stx CURSORTILEXPOS
+            stx CURSORXPOS
+            txa 
+            and #%00000111
+            cmp #%00000000
+            bne not_moving_right
+                ldx CURSORTILEXPOS
+                inx 
+                stx CURSORTILEXPOS
     not_moving_right:
     lda CURSORMOVEMENT
     and #%00000100
     beq not_moving_down
         ;moving_down:
-        ldx CURSORYPOS
-        inx 
-        stx CURSORYPOS
-        txa 
-        and #%00000111
-        cmp #%00000000
-        bne not_moving_down
-            ldx CURSORTILEYPOS
-            inx  
-            stx CURSORTILEYPOS
+        lda CURSORYPOS
+        cmp #$E6
+        beq not_moving_down
+            ldx CURSORYPOS
+            inx 
+            stx CURSORYPOS
+            txa 
+            and #%00000111
+            cmp #%00000000
+            bne not_moving_down
+                ldx CURSORTILEYPOS
+                inx  
+                stx CURSORTILEYPOS
     not_moving_down:
     lda CURSORMOVEMENT
     and #%00001000
     beq not_moving_left
         ;moving_left:
-        ldx CURSORXPOS
-        dex 
-        stx CURSORXPOS
-        txa 
-        and #%00000111
-        cmp #%00000111
-        bne not_moving_left
-            ldx CURSORTILEXPOS
+        lda CURSORXPOS
+        cmp #$00
+        beq not_moving_left
+            ldx CURSORXPOS
             dex 
-            stx CURSORTILEXPOS
+            stx CURSORXPOS
+            txa 
+            and #%00000111
+            cmp #%00000111
+            bne not_moving_left
+                ldx CURSORTILEXPOS
+                dex 
+                stx CURSORTILEXPOS
     not_moving_left:
 
     rts 
